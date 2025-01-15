fac15489908a0465198cbe14f8fe6f783c86596c
Rania <bellakhalrania12@gmail.com>
2025-01-15 14:33:51 +0100

    new change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Correct import
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
+    FooterComponent, // Declare only components, directives, and pipes
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule
+    CommonModule,
+    RouterModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
