3bbd1a2761ddca8130b4b239754b119c6622e9b3
ahmed-bs <medamed777@gmail.com>
2025-03-06 10:42:30 +0100

    changing the data of the user , and adding the parent calss and the child class

---
+++ src/app/core/models/parent.ts
@@ -0,0 +1,36 @@
+import { Child } from "./Child";
+
+export class Parent {
+    id: number;
+    image: string; // Photo du parent ou avatar
+    name: string; // Nom du parent/tuteur
+    relationship: string; // Lien avec l'enfant (Père, Mère, Tuteur légal)
+    contact: string; // Email ou numéro de téléphone
+    location: string; // Ville/Pays du parent
+    registrationDate: Date; // Date d'inscription à l'application
+    supportLevel: string; // Niveau d'implication (Actif, Occasionnel, En attente)
+    children: Child[]; // Liste des enfants suivis
+  
+    constructor(
+      id: number,
+      image: string,
+      name: string,
+      relationship: string,
+      contact: string,
+      location: string,
+      registrationDate: Date,
+      supportLevel: string,
+      children: Child[] = []
+    ) {
+      this.id = id;
+      this.image = image;
+      this.name = name;
+      this.relationship = relationship;
+      this.contact = contact;
+      this.location = location;
+      this.registrationDate = registrationDate;
+      this.supportLevel = supportLevel;
+      this.children = children;
+    }
+  }
+  
