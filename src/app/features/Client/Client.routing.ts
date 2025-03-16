193c0212fe33be44e0edf0e3ed569408a9353d99
ahmed-bs <medamed777@gmail.com>
2025-03-16 11:48:56 +0100

    adding evalutions part

--- src/app/features/Client/Client.routing.ts
+++ src/app/features/Client/Client.routing.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardClientComponent } from './dashboardClient/dashboardClient.component';
 import { ExploreComponent } from './explore/explore.component';
 import { Kids_profilesComponent } from './kids_profiles/kids_profiles.component';
+import { EvaluationsComponent } from './evaluations/evaluations.component';
 
 const routes: Routes = [
   { path:'',component:DashboardClientComponent },
   { path:'explore',component:ExploreComponent },
   { path:'Kids_profiles',component:Kids_profilesComponent },
+  { path:'evaluations',component:EvaluationsComponent },
 ];
 
 export const ClientRoutes = RouterModule.forChild(routes);
