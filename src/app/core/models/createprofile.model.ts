dbe4848298fbbf46178ecd563063336b60159d92
ahmed-bs <medamed777@gmail.com>
2025-04-27 20:28:14 +0100

    crud pick profile

---
+++ src/app/core/models/createprofile.model.ts
@@ -0,0 +1,29 @@
+export interface CreateProfileRequest {
+    first_name: string;
+    last_name: string;
+    birth_date: string;
+    gender?: 'M' | 'F' ;
+    diagnosis?: string;
+    notes?: string;
+  }
+  
+  export interface UpdateProfileRequest {
+    first_name?: string;
+    last_name?: string;
+    birth_date?: string;
+    gender?: 'M' | 'F' ;
+    diagnosis?: string;
+    notes?: string;
+    is_active?: boolean;
+  }
+  
+  export interface ShareProfileRequest {
+    shared_with_username: string;
+    permissions: ('view' | 'edit' | 'share')[];
+  }
+  
+  export interface ApiResponse<T> {
+    message?: string;
+    data?: T;
+    error?: string;
+  }
