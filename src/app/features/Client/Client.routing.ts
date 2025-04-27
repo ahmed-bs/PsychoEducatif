5ca4a270674d70b000f2c7fd126826e4dded68e7
Rania <bellakhalrania12@gmail.com>
2025-04-27 18:58:02 +0100

    add child_profile component

--- src/app/features/Client/Client.routing.ts
+++ src/app/features/Client/Client.routing.ts
@@ -5,12 +5,15 @@ import { EvaluationsComponent } from './evaluations/evaluations.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { PickProfileComponent } from 'src/app/shared/pick_profile/pick_profile.component';
+import { ChildProfileComponent } from './kids_profiles/kids_profiles.component';
+
 
 const routes: Routes = [
   { path:'',component:DashboardClientComponent },
   { path:'explore',component:ExploreComponent },
   { path:'calendar',component:CalendarComponent },
   { path:'Kids_profiles/:childId',component:PickProfileComponent },
+  { path:'profiles/:childId',component:ChildProfileComponent },
   { path:'evaluations/:categorie',component:EvaluationsComponent },
   { path: 'evaluations_configurations', loadChildren: () => import('./evaluationConfig/evaluationConfig.module').then(m => m.EvaluationConfigModule) },
   { path: 'quiz/:categorie', component: QuizComponent },
