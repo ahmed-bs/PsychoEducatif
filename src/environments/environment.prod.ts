d6aeb52cc4880f69a1da0a9f14c3d7184ad0ea4d
ahmed-bs <medamed777@gmail.com>
2026-02-02 02:17:46 +0100

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true ,
-  apiUrl: 'http://localhost:8001/',
- // apiUrl: 'http://31.97.199.25/api/',
+  //apiUrl: 'http://localhost:8000/',
+  apiUrl: 'http://31.97.199.25/api/',
 };
