59e24174f48f2d446577139a56a6f30279d9ecc1
ahmed-bs <medamed777@gmail.com>
2026-02-11 13:37:54 +0100

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true ,
   //apiUrl: 'http://localhost:8000/',
-  apiUrl: 'http://31.97.199.25/api/',
+  //apiUrl: 'http://31.97.199.25/api/',
+   apiUrl: '/api'  ,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8001/',
-apiUrl: 'http://31.97.199.25/api/'
+   //apiUrl: 'http://31.97.199.25/api/'
+    apiUrl: '/api'  ,
 };
 
 /*
